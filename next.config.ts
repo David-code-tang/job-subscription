0846e1254714ae1fe2d4a5cf9e0bee82fed94a51
David-code-tang <davidtang.20@outlook.com>
2026-01-12 21:12:25 +0800

    Configure Next.js to handle LESS files from AntV S2
    - Add webpack configuration to compile .less files
    - Install less-loader and style-loader
    - Disable Turbopack to avoid LESS compilation issues
    - This fixes the build error with @antv/s2-react

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Disable Turbopack to avoid LESS file compilation issues
+  // Turbopack in Next.js 16 doesn't handle .less files from @antv/s2-react
+  experimental: {
+    turbo: undefined,
+  },
+  // Configure webpack to handle LESS files
+  webpack: (config, { isServer }) => {
+    // Handle .less files from @antv/s2-react
+    config.module.rules.push({
+      test: /\.less$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'less-loader',
+          options: {
+            lessOptions: {
+              modifyVars: {
+                // Can override AntV S2 theme variables here
+              },
+              javascriptEnabled: true,
+            },
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
